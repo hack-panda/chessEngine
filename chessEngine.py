8e4cd9647e05e14c4bea442675c05147b4479400
hack-panda <dthesec@gmail.com>
2021-03-19 14:20:14 +0530

    added the functionality to undo a move and created to a framework to work on generating valid possible moves

--- chessEngine.py
+++ chessEngine.py
@@ -21,11 +21,65 @@ class GameState():
         self.whiteToMove = True
         self.moveLog = []
 
+    """ Takes Move as a parameter and executes it.
+    Will not work for en-passant,pawn promition and castling"""
+
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.moveLog.append(move)  # log the move so we can undo it later
         self.whiteToMove = not self.whiteToMove  # swap the chance of player
+
+    """
+    Undo the last move made
+    """
+
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+    """ 
+    All moves considering checks
+    """
+
+    def getValidMoves(self):
+        # for now we will not consider for checks
+        return self.getAllPossibleMoves()
+
+    """
+    All possible moves for a piece without considering checks 
+    """
+
+    def getAllPossibleMoves(self):
+        moves = [Move((6, 4), (4, 4), self.board)]
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if(turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
+                    peice = self.board[r][c][1]
+                    if peice == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif peice == 'R':
+                        self.getRookMoves(r, c, moves)
+
+        return moves
+
+    """
+    Get all the pawn moves for a pawn at (row,column)
+    """
+
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    """
+    Get all the rook moves for a rook at (row,column)
+    """
+
+    def getRookMoves(self, r, c, moves):
+        pass
 
 
 class Move():
@@ -46,6 +100,18 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * \
+            100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+
+    """
+    Overriding the equals method 
+    """
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
