28b4e05e1aca817bee5dfcd898c80d2509452a9a
hack-panda <dthesec@gmail.com>
2021-03-28 10:34:38 +0530

    added bestMove AI

--- moveAI.py
+++ moveAI.py
@@ -1,5 +1,54 @@
 import random
 
+pieceScore = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1}
+CHECKMATE = 1000
+STALEMATE = 0
+
 
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
+
+
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMove else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        opponentMoves = gs.getValidMoves()
+        opponentMaxScore = -CHECKMATE
+        for opponentMove in opponentMoves:
+            gs.makeMove(opponentMove)
+            if gs.checkMate:
+                score = -turnMultiplier * CHECKMATE
+            elif gs.staleMate:
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.board)
+
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+            gs.undoMove()
+
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
+
+        gs.undoMove()
+
+    return bestPlayerMove
+
+
+""" Calculate the Score of board based on material """
+
+
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for piece in row:
+            if piece[0] == 'w':
+                score += pieceScore[piece[1]]
+            elif piece[0] == 'b':
+                score -= pieceScore[piece[1]]
+    return score
